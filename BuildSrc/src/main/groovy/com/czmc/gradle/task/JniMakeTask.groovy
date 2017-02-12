1cc7eaa630fed2f0fae0a32f40d4d380f0d365d6
czmz <zxc6355533@163.com>
2017-02-12 22:15:40 +0800

    add run log!

--- BuildSrc/src/main/groovy/com/czmc/gradle/task/JniMakeTask.groovy
+++ BuildSrc/src/main/groovy/com/czmc/gradle/task/JniMakeTask.groovy
@@ -12,8 +12,11 @@ public class JniMakeTask extends Exec {
     protected void exec() {
         def compileSdk = "${project.sdkDir}/platforms/${project.android.compileSdkVersion}/android.jar;${project.sdkDir}/extras/android/support/v7/appcompat/libs/*;"
         def targetFile = project.buildDir.absolutePath+"\\intermediates\\classes\\debug"
+        System.out.println("compile SDK:"+compileSdk)
+        System.out.println("compile extra path:"+project.JniMaker.entraClassPath)
+        System.out.println("target file:"+targetFile)
         commandLine 'cmd',"/c","javah -d  ${project.JniMaker.buildSrc} -classpath ${targetFile};${compileSdk};${project.JniMaker.entraClassPath} ${project.JniMaker.targetClass}"
-        super.exec()
         System.out.println("remoteConnect Exec..");
+        super.exec()
     }
 }
